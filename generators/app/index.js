41df118f74396a8c66522087bd923ee874dbe32f
Brendan McKenzie <hello@brendanmckenzie.com>
2017-02-03 08:18:29 +1100

    Added .vscode

--- generators/app/index.js
+++ generators/app/index.js
@@ -30,6 +30,7 @@ module.exports = Generator.extend({
     this.fs.copyTpl(this.templatePath('src/Shared/**/*'), this.destinationPath('src/' + this.props.namespace), this.props);
     this.fs.copyTpl(this.templatePath('src/ContentDelivery/**/*'), this.destinationPath('src/' + this.props.namespace + '.ContentDelivery'), this.props);
     this.fs.copyTpl(this.templatePath('src/ContentManagement/**/*'), this.destinationPath('src/' + this.props.namespace + '.ContentManagement'), this.props);
+    this.fs.copyTpl(this.templatePath('.vscode/**/*'), this.destinationPath('.vscode/'), this.props);
     this.fs.copy(this.templatePath('global.json'), this.destinationPath('global.json'));
     this.fs.copy(this.templatePath('.gitignore'), this.destinationPath('.gitignore'));
     // TODO: remove this once the package is released on NuGet
